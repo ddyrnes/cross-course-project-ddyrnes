b080ddefc22fc1c42cb3f8b4430025784735cff6
ddyrnes <dev.dyrnes@gmail.com>
2022-10-05 20:42:57 +0200

    Added jackets to array + deleted hotjar links

---
+++ js/cart/jackets.js
@@ -0,0 +1,55 @@
+let jackets = [
+  {
+    name: "Jacket 1",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Varius tincidunt non cursus id fusce.",
+    price: "699.00",
+    image: "/images/jacket1yellow.jpg",
+    popular: "true",
+    onsale: "true",
+  },
+  {
+    name: "Jacket 2",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Varius tincidunt non cursus id fusce.",
+    price: "599.00",
+    image: "/images/jacket2red.jpg",
+  },
+  {
+    name: "Jacket 3",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Varius tincidunt non cursus id fusce.",
+    price: "799.00",
+    image: "/images/jacket3green.jpg",
+    popular: "true",
+  },
+  {
+    name: "Jacket 4",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Varius tincidunt non cursus id fusce.",
+    price: "899.00",
+    image: "/images/jacket4black.jpg",
+  },
+  {
+    name: "Jacket 5",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Varius tincidunt non cursus id fusce.",
+    price: "1199.00",
+    image: "/images/jacket5.jpg",
+    popular: "true",
+  },
+  {
+    name: "Jacket 6",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Varius tincidunt non cursus id fusce.",
+    price: "749.00",
+    image: "/images/jacket6.jpg",
+  },
+  {
+    name: "Jacket 7",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Varius tincidunt non cursus id fusce.",
+    price: "899.00",
+    image: "/images/jacket7.jpg",
+  },
+  {
+    name: "Jacket 8",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Varius tincidunt non cursus id fusce.",
+    price: "499.00",
+    image: "/images/jacket8.jpg",
+  },
+];
+console.log(jackets[3].price);
